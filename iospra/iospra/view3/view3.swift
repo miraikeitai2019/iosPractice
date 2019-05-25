12666da5583bff2b10a67bd6bbfc2823f3ce4920
Fuyan777 <fuyamiwa713@gmail.com>
2019-05-25 14:10:22 +0900

    [add]土台追加

---
+++ iospra/iospra/view3/view3.swift
@@ -0,0 +1,30 @@
+//
+//  view3.swift
+//  iospra
+//
+//  Created by 山田楓也 on 2019/05/25.
+//  Copyright © 2019 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class view3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
