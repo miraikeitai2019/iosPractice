01cf9d22e06520087d9d3014390a10f9734acf57
YuriIto <b1017208@fun.ac.jp>
2019-05-26 22:26:41 +0900

    修正：一度停止したタイマーを再開させる

--- iospra/iospra/view3/view3.swift
+++ iospra/iospra/view3/view3.swift
@@ -14,28 +14,28 @@ class view3: UIViewController {
     
     var startTime: TimeInterval? = nil //Double型
     var timer = Timer() //止めた時に返す値を初期化
-    
+    var elapsedTime: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @objc func update(){
         //2001.1.1 00:00:00
-//        print(Date.timeIntervalSinceReferenceDate)
+        //        print(Date.timeIntervalSinceReferenceDate)
         if let startTime = self.startTime {
-            let t: Double = Date.timeIntervalSinceReferenceDate - startTime
+            let t: Double = Date.timeIntervalSinceReferenceDate - startTime + self.elapsedTime
             //経過時間　＝　今の基準日からの経過秒数　ー　startTime
-//        print(t)
+            //        print(t)
             let min = Int(t / 60)
             let sec = Int(t) % 60
             let msec = Int((t - Double(sec)) * 100.0)//ミリ秒
             self.timerLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
         }
     }
-
+    
     @IBAction func startTimer(_ sender: Any) {
         self.startTime = Date.timeIntervalSinceReferenceDate
         self.timer = Timer.scheduledTimer(
@@ -47,22 +47,26 @@ class view3: UIViewController {
     }
     
     @IBAction func stopTimer(_ sender: Any) {
+        if let startTime = self.startTime {
+            self.elapsedTime += Date.timeIntervalSinceReferenceDate - startTime
+        }
         self.timer.invalidate() //startTimerのTimer.schedulesTimerの返り値
     }
     
     @IBAction func resetTimer(_ sender: Any) {
         self.startTime = nil //リセットを押すと0になる
         self.timerLabel.text = "00:00:00"
+        self.elapsedTime = 0.0
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
     */
 
 }
